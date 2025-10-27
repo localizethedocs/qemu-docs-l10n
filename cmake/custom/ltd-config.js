088e761117061bc3057b9c84dd245d40ba226f89
Haowei Hsu <hwhsu1231@localizethedocs.org>
2025-10-27 13:33:21 +0800

    ✨ feat(init): initialize branch 'main' (#1)
    Initialized the 'main' branch with the following files:
    
    - Added document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Added GitHub workflow files: ci-check-pr-status,..., etc.
    - Added GitHub issue templates: language_request.yml,..., etc.
    - Added Git config files: .gitmodules, .gitignore.
    - Added Git submodules: cmake/modules, cmake/plugins.
    - Added CMake script files: CMakeLists.txt, cmake/**/* files.
    - Added config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["10",      "Release 10"],
+    ["9",       "Release 9"],
+    ["8",       "Release 8"],
+    ["7",       "Release 7"],
+    ["6",       "Release 6"],
+    ["5",       "Release 5"],
+    ["4",       "Release 4"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/qemu-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/qemu-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/qemu-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/qemu-docs-l10n"],
+  ]
+};
