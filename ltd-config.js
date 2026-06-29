b1e5786248a89423fb5dcaccb2d8f33a3bb4c926
ltdrobot <ltdrobot@localizethedocs.org>
2026-06-29 13:59:35 +0000

    Deploying to pages from @ localizethedocs/qemu-docs-l10n@48969564ca45739ee45468b80d467d4ca271a704 🚀

---
+++ ltd-config.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["10.1",    "Release 10.1"],
+    ["10.0",    "Release 10.0"],
+    ["9.2",     "Release 9.2"],
+    ["9.1",     "Release 9.1"],
+    ["9.0",     "Release 9.0"],
+    ["8.2",     "Release 8.2"],
+    ["8.1",     "Release 8.1"],
+    ["8.0",     "Release 8.0"],
+    ["7.2",     "Release 7.2"],
+    ["7.1",     "Release 7.1"],
+    ["7.0",     "Release 7.0"],
+    ["6.2",     "Release 6.2"],
+    ["6.1",     "Release 6.1"],
+    ["6.0",     "Release 6.0"],
+    ["5.2",     "Release 5.2"],
+    ["5.1",     "Release 5.1"],
+    ["5.0",     "Release 5.0"],
+    ["4.2",     "Release 4.2"],
+    ["4.1",     "Release 4.1"],
+    ["4.0",     "Release 4.0"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/qemu-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/qemu-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/qemu-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/qemu-docs-l10n"],
+  ]
+};
